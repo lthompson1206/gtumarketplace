16d4e013cd18f9bb13d9033595c3764a995e32d1
CodeWorld3 <lthompson1206@gmail.com>
2024-08-19 13:35:38 -0400

    Added Image folder and dummy placeholder image for menu item

--- src/components/MealItem.jsx
+++ src/components/MealItem.jsx
@@ -1,7 +1,7 @@
 export default function MealItem({ meal }) {
     return <li className="meal-item">
         <article>
-            <img src={`/backend/data/${meal.image}`} alt={meal.name} />
+            <img src={`./backend/data/${meal.image}`} alt={meal.name} />
             <div>
                 <h3>{meal.name}</h3>
                 <p className="meal-item-price">{meal.price}</p>
