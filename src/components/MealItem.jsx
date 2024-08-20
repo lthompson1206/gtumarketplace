3ca20236368d80fa57513e8f16d52ee114c489bf
CodeWorld3 <lthompson1206@gmail.com>
2024-08-20 16:48:25 -0400

    made slight edit to the last push

--- src/components/MealItem.jsx
+++ src/components/MealItem.jsx
@@ -6,7 +6,9 @@ export default function MealItem({ meal }) {
             <img src={`./backend/data/${meal.image}`} alt={meal.name} />
             <div>
                 <h3>{meal.name}</h3>
-                <p className="meal-item-price">{currencyFormatter.format(meal.price)}</p>
+                <p className="meal-item-price">
+                    {currencyFormatter.format(meal.price)}
+                    </p>
                 <p className="meal-item-description">{meal.description}</p>
             </div>
             <p className="meal-item-actions">
